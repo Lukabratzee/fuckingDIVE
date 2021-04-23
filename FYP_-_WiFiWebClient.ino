77c50aded1b44d8311cf50e0a493ba1f1212f227
Lukabratzee <lukabratzee@icloud.com>
2021-04-23 02:48:13 +0100

    Add files via upload

---
+++ FYP_-_WiFiWebClient.ino
@@ -0,0 +1,142 @@
+#include <SPI.h>
+#include <WiFiNINA.h>
+#include <ArduinoJson.h>
+#include <ArduinoHttpClient.h>
+
+
+// Configure the pins used for the ESP32 connection
+#if defined(ADAFRUIT_FEATHER_M4_EXPRESS) || \
+  defined(ADAFRUIT_FEATHER_M0_EXPRESS) || \
+  defined(ADAFRUIT_FEATHER_M0) || \
+  defined(ARDUINO_AVR_FEATHER32U4) || \
+  defined(ARDUINO_NRF52840_FEATHER) || \
+  defined(ADAFRUIT_ITSYBITSY_M0) || \
+  defined(ADAFRUIT_ITSYBITSY_M4_EXPRESS) || \
+  defined(ARDUINO_AVR_ITSYBITSY32U4_3V) || \
+  defined(ARDUINO_NRF52_ITSYBITSY)
+  // Configure the pins used for the ESP32 connection
+  #define SPIWIFI       SPI  // The SPI port
+  #define SPIWIFI_SS    13   // Chip select pin
+  #define ESP32_RESETN  12   // Reset pin
+  #define SPIWIFI_ACK   11   // a.k.a BUSY or READY pin
+  #define ESP32_GPIO0   -1
+#elif defined(ARDUINO_AVR_FEATHER328P)
+  #define SPIWIFI       SPI  // The SPI port
+  #define SPIWIFI_SS     4   // Chip select pin
+  #define ESP32_RESETN   3   // Reset pin
+  #define SPIWIFI_ACK    2   // a.k.a BUSY or READY pin
+  #define ESP32_GPIO0   -1
+#elif defined(TEENSYDUINO)
+  #define SPIWIFI       SPI  // The SPI port
+  #define SPIWIFI_SS     5   // Chip select pin
+  #define ESP32_RESETN   6   // Reset pin
+  #define SPIWIFI_ACK    9   // a.k.a BUSY or READY pin
+  #define ESP32_GPIO0   -1
+#elif defined(ARDUINO_NRF52832_FEATHER)
+  #define SPIWIFI       SPI  // The SPI port
+  #define SPIWIFI_SS    16   // Chip select pin
+  #define ESP32_RESETN  15   // Reset pin
+  #define SPIWIFI_ACK    7   // a.k.a BUSY or READY pin
+  #define ESP32_GPIO0   -1
+#elif !defined(SPIWIFI_SS)   // if the wifi definition isnt in the board variant
+  // Don't change the names of these #define's! they match the variant ones
+  #define SPIWIFI       SPI
+  #define SPIWIFI_SS    10   // Chip select pin
+  #define SPIWIFI_ACK    7   // a.k.a BUSY or READY pin
+  #define ESP32_RESETN   5   // Reset pin
+  #define ESP32_GPIO0   -1   // Not connected
+#endif
+
+#include "arduino_secrets.h"
+///////please enter your sensitive data in the Secret tab/arduino_secrets.h
+char ssid[] = SECRET_SSID;        // your network SSID (name)
+char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
+int keyIndex = 0;            // your network key Index number (needed only for WEP)
+
+int status = WL_IDLE_STATUS;
+// if you don't want to use DNS (and reduce your sketch size)
+// use the numeric IP instead of the name for the server:
+//IPAddress server(74,125,232,128);  // numeric IP for Google (no DNS)
+
+char server[] = "192.168.1.92";    // name address for adafruit test
+String path   = "/data";
+
+char serverAddress[] = "192.168.1.92";  // server address
+int port = 5000;
+
+WiFiClient wifi;
+HttpClient client = HttpClient(wifi, serverAddress, port);
+
+
+
+void setup() {
+
+  String json;
+
+  Serial.begin(9600);
+  while (!Serial) continue;
+
+  //JSON data, this is correct. I just need to send this to an IP
+  DynamicJsonDocument doc(1024);
+  doc["sensor"] = "Temp";
+  doc["value"] = analogRead(0);
+  serializeJson(doc, Serial);
+  serializeJson(doc, json);
+
+  // check for the WiFi module:
+  WiFi.setPins(SPIWIFI_SS, SPIWIFI_ACK, ESP32_RESETN, ESP32_GPIO0, &SPIWIFI);
+  while (WiFi.status() == WL_NO_MODULE) {
+    Serial.println("Communication with WiFi module failed!");
+    // don't continue
+    delay(1000);
+  }
+
+  // attempt to connect to Wifi network:
+  Serial.print("Attempting to connect to SSID: ");
+  Serial.println(ssid);
+  // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+  do {
+    status = WiFi.begin(ssid, pass);
+    delay(100);     // wait until connection is ready!
+  } while (status != WL_CONNECTED);
+
+  Serial.println("Connected to wifi");
+  printWifiStatus();
+
+  
+  
+  Serial.println("Connected to server");
+  String contentType = "application/json";
+//  values = "temp=" + analogRead(0);
+//  Serial.println(values);
+//  String postData = values;
+
+  client.post(path, contentType, json);
+  Serial.println(json);
+}
+
+
+void loop() {
+if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
+
+
+}
+}
+
+
+void printWifiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your board's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
